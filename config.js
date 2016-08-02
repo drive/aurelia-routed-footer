914c51bd98037a7239f0511a4429ec85b3578815
Michael Malone <michael.malone@drivesoftware.co.nz>
2016-08-02 16:11:28 +1200

    Updating to aurelia-binding 1.0.1

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ System.config({
   },
 
   map: {
-    "aurelia-binding": "npm:aurelia-binding@1.0.0",
+    "aurelia-binding": "npm:aurelia-binding@1.0.1",
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0",
     "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0",
     "aurelia-pal": "npm:aurelia-pal@1.0.0",
     "aurelia-router": "npm:aurelia-router@1.0.2",
     "aurelia-task-queue": "npm:aurelia-task-queue@1.0.0",
     "aurelia-templating": "npm:aurelia-templating@1.0.0",
-    "npm:aurelia-binding@1.0.0": {
+    "npm:aurelia-binding@1.0.1": {
       "aurelia-logging": "npm:aurelia-logging@1.0.0",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0",
       "aurelia-pal": "npm:aurelia-pal@1.0.0",
@@ -51,7 +51,7 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.0.0"
     },
     "npm:aurelia-templating@1.0.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0",
+      "aurelia-binding": "npm:aurelia-binding@1.0.1",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0",
       "aurelia-loader": "npm:aurelia-loader@1.0.0",
       "aurelia-logging": "npm:aurelia-logging@1.0.0",
