b0541367fa8ca5ddb0ead41a2a3b27671f2f040d
Simon Fox <simon.fox@drivesoftware.co.nz>
2016-10-06 16:55:47 +1300

    upgrade aurelia deps

--- dist/system/index.js
+++ dist/system/index.js
@@ -4,6 +4,23 @@ System.register(['aurelia-router', './updatefooterstep'], function (_export, _co
   "use strict";
 
   var Router, PipelineProvider, UpdateFooterStep;
+  function configure(aurelia) {
+    Router.prototype.footer = null;
+    Router.prototype.registerFooterViewPort = function (footer) {
+      this.footer = footer;
+    };
+    Router.prototype.setFooter = function (footerModule) {
+      this.footer.setFooter(footerModule);
+    };
+
+    var filters = aurelia.container.get(PipelineProvider);
+    filters.addStep('precommit', UpdateFooterStep);
+
+    aurelia.globalResources(['./footerview']);
+  }
+
+  _export('configure', configure);
+
   return {
     setters: [function (_aureliaRouter) {
       Router = _aureliaRouter.Router;
@@ -11,23 +28,6 @@ System.register(['aurelia-router', './updatefooterstep'], function (_export, _co
     }, function (_updatefooterstep) {
       UpdateFooterStep = _updatefooterstep.UpdateFooterStep;
     }],
-    execute: function () {
-      function configure(aurelia) {
-        Router.prototype.footer = null;
-        Router.prototype.registerFooterViewPort = function (footer) {
-          this.footer = footer;
-        };
-        Router.prototype.setFooter = function (footerModule) {
-          this.footer.setFooter(footerModule);
-        };
-
-        var filters = aurelia.container.get(PipelineProvider);
-        filters.addStep('precommit', UpdateFooterStep);
-
-        aurelia.globalResources(['./footerview']);
-      }
-
-      _export('configure', configure);
-    }
+    execute: function () {}
   };
 });
