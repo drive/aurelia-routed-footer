72be994f7a17f025f92836dd0ebc4e607af95d11
Simon Fox <simon.fox@drivesoftware.co.nz>
2017-02-27 21:35:56 +1300

    upgrade aurelia deps

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ System.config({
   },
 
   map: {
-    "aurelia-binding": "npm:aurelia-binding@1.1.1",
+    "aurelia-binding": "npm:aurelia-binding@1.2.0",
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
     "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.1",
     "aurelia-pal": "npm:aurelia-pal@1.3.0",
     "aurelia-router": "npm:aurelia-router@1.2.1",
     "aurelia-task-queue": "npm:aurelia-task-queue@1.2.0",
-    "aurelia-templating": "npm:aurelia-templating@1.2.0",
-    "npm:aurelia-binding@1.1.1": {
+    "aurelia-templating": "npm:aurelia-templating@1.3.0",
+    "npm:aurelia-binding@1.2.0": {
       "aurelia-logging": "npm:aurelia-logging@1.3.0",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
       "aurelia-pal": "npm:aurelia-pal@1.3.0",
@@ -50,8 +50,8 @@ System.config({
     "npm:aurelia-task-queue@1.2.0": {
       "aurelia-pal": "npm:aurelia-pal@1.3.0"
     },
-    "npm:aurelia-templating@1.2.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.1.1",
+    "npm:aurelia-templating@1.3.0": {
+      "aurelia-binding": "npm:aurelia-binding@1.2.0",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
       "aurelia-loader": "npm:aurelia-loader@1.0.0",
       "aurelia-logging": "npm:aurelia-logging@1.3.0",
