700a60a1bec23205df30ef043855086faf9edd2f
Simon Fox <simon.fox@drivesoftware.co.nz>
2017-02-27 21:27:01 +1300

    update aurelia router dep to 1.2.1

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ System.config({
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
     "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.1",
     "aurelia-pal": "npm:aurelia-pal@1.3.0",
-    "aurelia-router": "npm:aurelia-router@1.2.0",
+    "aurelia-router": "npm:aurelia-router@1.2.1",
     "aurelia-task-queue": "npm:aurelia-task-queue@1.2.0",
     "aurelia-templating": "npm:aurelia-templating@1.2.0",
     "npm:aurelia-binding@1.1.1": {
@@ -39,7 +39,7 @@ System.config({
     "npm:aurelia-route-recognizer@1.1.0": {
       "aurelia-path": "npm:aurelia-path@1.1.1"
     },
-    "npm:aurelia-router@1.2.0": {
+    "npm:aurelia-router@1.2.1": {
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
       "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.1",
       "aurelia-history": "npm:aurelia-history@1.0.0",
